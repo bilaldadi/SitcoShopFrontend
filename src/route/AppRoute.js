51ed7d4694e96849d294e4f9dcd9d6b7a4acb00f
Bilal <bilaldadi963@gmail.com>
2023-07-16 12:56:00 +0300

    Login Page for desktop

--- src/route/AppRoute.js
+++ src/route/AppRoute.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react'
 import { Route,Switch } from 'react-router'
 import HomePage from '../pages/HomePage'
+import UserLoginPage from '../pages/UserLoginPage'
+
 
 class AppRoute extends Component {
   render() {
@@ -8,6 +10,7 @@ class AppRoute extends Component {
       <Fragment>
         <Switch>
             <Route exact path="/" component={HomePage} />
+            <Route exact path="/login" component={UserLoginPage} />
         </Switch>
       </Fragment>
     )
