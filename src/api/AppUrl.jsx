4660dc15da0f1a6d1b97c896c0da1da7378db628
bilaldadi <belaldaady2@gmail.com>
2023-07-17 00:44:57 +0300

    Connect FE with the BE

---
+++ src/api/AppUrl.jsx
@@ -0,0 +1,8 @@
+class AppUrl  {
+  
+    static BaseUrl = "http://127.0.0.1:8000/api";
+
+    static VisitorDetails = this.BaseUrl +"/getvisitor"
+}
+
+export default AppUrl
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,16 +6,22 @@ import NewArrival from '../components/home/NewArrival'
 import HomeTop from '../components/home/HomeTop'
 import HomeTopMobile from '../components/home/HomeTopMobile'
 import NavMenuDsektop from '../components/common/NavMenuDesktop'
-
 import NavMenuMobile from '../components/common/NavMenuMobile'
 import FooterDesktop from '../components/common/FooterDesktop'
 import FooterMobile from '../components/common/FooterMobile'
+import axios from 'axios'
+import AppUrl from '../api/AppUrl'
 
 
 class HomePage extends Component {
 
   componentDidMount(){
     window.scroll(0,0)
+    this.GetVisitorDetails();
+  }
+
+  GetVisitorDetails = () => {
+    axios.get(AppUrl.VisitorDetails).then().catch();
   }
 
   render() {
